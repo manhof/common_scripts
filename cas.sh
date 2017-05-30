9bafdfb257fc3a5a6ac0cedc30c0cdd9dae0c28b
manhof <manhof@users.noreply.github.com>
2017-05-30 14:27:08 -0400

    Update cas.sh

--- cas.sh
+++ cas.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-serverbuild= gawk -F= '/^ID=/{print $2}' /etc/os-release
+gawk -F= '/^ID=/{print $2}' /etc/os-release > /home/id.txt
+serverbuild=$(cat /home/id.txt)
+echo " This is the Server Build: " $serverbuild >> /home/test
 #Certs need to .cer pem files which will be converted to .crt pem files
 for var in "$@"
 do	
