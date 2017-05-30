82ee652d444e3af969639672837d15e013fbfd1e
manhof <manhof@users.noreply.github.com>
2017-05-30 13:47:24 -0400

    Update vol_mount.sh
    updating mount directory

--- vol_mount.sh
+++ vol_mount.sh
@@ -2,7 +2,7 @@
 mount_dir=$1
 sgdisk --largest-new=1 /dev/vdb 
 mkfs.xfs /dev/vdb1
-mkdir /mnt/cockroach
+mkdir $mount_dir
 mount -t xfs /dev/vdb1 $mount_dir
 echo "/dev/vdb1 $mount_dir xfs defaults 0 0" >> /etc/fstab
 echo "Storage has been added to system on $mount_dir" >> /home/test
