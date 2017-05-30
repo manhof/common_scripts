b0663fd1b21ee4695dab60dc9c76469be4b8f5dc
manhof <manhof@users.noreply.github.com>
2017-05-30 14:26:27 -0400

    Update ntp.sh

--- ntp.sh
+++ ntp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gawk -F= '/^ID=/{print $2}' /etc/os-release > /home/id.txt
-serverbuild=$(cat /hhome/id.txt)
+serverbuild=$(cat /home/id.txt)
 echo " This is the Server Build: " $serverbuild >> /home/test
 if [[ $serverbuild == *"ubuntu"* ]]
  then
