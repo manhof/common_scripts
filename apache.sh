7f88baa57bc7222b217a63397dcc24ac9e8dfaab
manhof <manhof@users.noreply.github.com>
2017-06-30 08:17:51 -0400

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -4,16 +4,16 @@ gawk -F= '/^ID=/{print $2}' /etc/os-release > /home/id.txt
 serverbuild=$(cat /home/id.txt)
 if [[ $serverbuild == *"ubuntu"* ]]
  then
-	add-apt-repository ppa:ondrej/apache2
-	apt update -y
-	apt install apache2
-	echo "apache2 has been installed" >> /home/test
+  add-apt-repository ppa:ondrej/apache2
+  apt update -y
+  apt install apache2
+  echo "apache2 has been installed" >> /home/test
 elif [[ $serverbuild == *"centos"* ]]
  then
-	dnf install httpd -y
-	dnf install mod_ssl -y
-	echo "apache2 has been installed" >> /home/test
+  dnf install httpd -y
+  dnf install mod_ssl -y
+  echo "apache2 has been installed" >> /home/test
 else
-    echo "Cannot determine Build Type... Exiting" >> /home/test
-	exit 3
+ echo "Cannot determine Build Type... Exiting" >> /home/test
+ exit 3
 fi	   
